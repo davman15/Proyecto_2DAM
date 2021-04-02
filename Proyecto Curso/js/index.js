8c4d188bae2577ec225a4052ac6c487d834cca1f
davman15 <davidmanrique15@gmail.com>
2021-04-02 22:49:03 +0200

    Hicimos el Registro
    Mas retoques al index e iniciar Sesion

--- Proyecto Curso/js/index.js
+++ Proyecto Curso/js/index.js
@@ -8,7 +8,7 @@ function mostrarAnimacionIzquierdaDerecha(){
     //Recorro con el for los elementos y se va a repetir las veces que haya un elemento
     for(var i=0; i<animado.length;i++){
         let alturaAnimado=animado[i].offsetTop;
-        if(alturaAnimado - 500<scrollTop){
+        if(alturaAnimado - 300<scrollTop){
             animado[i].style.opacity=1;
             animado[i].classList.add("mostrarArriba");
         }
@@ -20,7 +20,7 @@ function mostrarAnimacionDerechaIzquierda(){
     //Recorro con el for los elementos y se va a repetir las veces que haya un elemento
     for(var i=0; i<animadoIzquierda.length;i++){
         let alturaAnimado=animadoIzquierda[i].offsetTop;
-        if(alturaAnimado - 500<scrollTop){
+        if(alturaAnimado - 300<scrollTop){
             animadoIzquierda[i].style.opacity=1;
             animadoIzquierda[i].classList.add("mostrarIzquierda");
         }
